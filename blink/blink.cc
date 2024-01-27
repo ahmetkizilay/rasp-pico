0b2eaee194e1e3c9ba74ef497c96597fb8713789
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-01-27 23:00:31 +0000

    initial working blink project

---
+++ blink/blink.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+int main() {
+    stdio_init_all();
+
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while(1) {
+        gpio_put(LED_PIN, 1);
+        printf("LED ON!\n");
+        sleep_ms(500);
+
+        gpio_put(LED_PIN, 0);
+        printf("LED OFF!\n");
+        sleep_ms(500);
+    }
+}
