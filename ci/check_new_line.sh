e0818b9d3ce6a1ae92d5c7e3da538390b2118173
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-02-06 01:47:08 +0000

    Check for new lines

---
+++ ci/check_new_line.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+# Check for new line at the end the file
+
+# collect the list of files changed
+
+files_changed=()
+while read line
+do
+  files_changed+=( "$line" )
+done
+
+num_failed=0
+# process each file
+for f in ${files_changed[@]}; do
+  echo -n "$f"
+  if [ "$(tail -c 1 "$f" | wc -l)" -eq 0 ]; then
+    echo " ... failed"
+    num_failed=$((num_failed+1))
+  else 
+    echo " ... passing"
+  fi
+done
+
+if [ $num_failed -gt 0 ]; then
+  echo "Some files failed new_line check!"
+  exit 1
+fi
