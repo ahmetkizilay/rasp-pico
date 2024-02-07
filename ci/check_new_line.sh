70437b9311a24727581b7ed09b4d729463a2b4d5
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-02-07 02:28:58 +0000

    add red cross mark and green tick mark

--- ci/check_new_line.sh
+++ ci/check_new_line.sh
@@ -12,10 +12,12 @@ num_failed=0
 for f in ${files_changed[@]}; do
   echo -n "$f"
   if [ "$(tail -c 1 "$f" | wc -l)" -eq 0 ]; then
-    echo " ... failed"
+    # red cross mark
+    printf " \033[31m\xE2\x9D\x8C\033[0m\n"
     num_failed=$((num_failed+1))
-  else 
-    echo " ... passing"
+  else
+    # green tick mark
+    printf " \033[32m\xE2\x9C\x94\033[0m\n"
   fi
 done
 
