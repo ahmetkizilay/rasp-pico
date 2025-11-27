52e16c06568873e15fd50e3fd94e2055f65c9f37
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2025-11-27 16:34:56 -0500

    fix compilation for pico and pico2

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Some experimental library and executables for the Raspberry PI Pico.
 
 [![](https://github.com/jpoehnelt/in-solidarity-bot/raw/main/static//badge-for-the-badge.png)](https://github.com/apps/in-solidarity)
+
+## Building
+
+### Changing board name
+
+1. Run `CMake: Delete Cache and Reconfigure`
+2. Run `CMake: Clean Rebuild`
--- demos/pio_blink/hello.cc
+++ demos/pio_blink/hello.cc
@@ -2,17 +2,16 @@
 #include "hardware/pio.h"
 #include "hello.pio.h"
 
-#define LED 13
-
 int main() {
     // RP2040 has 2 PIOs with 4 state machines.
+    // RP2350 (Pico 2) has 3 PIOs with 4 state machines.
     PIO pio = pio0;
 
     // Load the program into PIO's instructions memory
     uint offset = pio_add_program(pio, &hello_program);
 
     uint sm = pio_claim_unused_sm(pio, true);
-    hello_program_init(pio, sm, offset, LED);
+    hello_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
 
     while (true) {
         pio_sm_put_blocking(pio, sm, 1);
